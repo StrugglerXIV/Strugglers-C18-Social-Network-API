248fdeb0a1b4301f53fd09672847c4ee2c867246
StrugglerXIV <arbiterdenis@gmail.com>
2023-08-05 14:40:07 -0400

    Pushing initial structure and config

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const db = require("./config/config.js");
+const routes = require("./routes");
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //These format the data to be read by the server, if we didnt have this req.body and req.params would be undefined
+app.use(routes);
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
