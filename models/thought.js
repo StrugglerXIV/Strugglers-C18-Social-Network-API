cc15e27ca212c69a63978a9b77bbe73edd2d5da9
StrugglerXIV <arbiterdenis@gmail.com>
2023-08-05 14:49:52 -0400

    Added models for user and thought

--- models/thought.js
+++ models/thought.js
@@ -1,1 +1,41 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
+const reactionSchema = require("./Reaction");
+const dateFormat = require("../utils/dateFormat");
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    // Use ReactionsSchema to validate data for a reply
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    // prevents virtuals from creating duplicate of _id as `id`
+    id: false,
+  }
+);
+
+// get total count of reactions and replies on retrieval
+ThoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("Thought", ThoughtSchema);
+
+module.exports = Thought;
